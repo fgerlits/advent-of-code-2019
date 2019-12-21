47c207afa25add4ce02c73cafea71e9a9f7702af
Ferenc Gerlits <fgerlits@gmail.com>
2019-12-21 10:37:02 +0100

    Day 21 puzzle two

--- 21/one.rb
+++ 21/one.rb
@@ -4,7 +4,7 @@ require_relative '../common/intcode_computer'
 
 PROGRAM = open('input').read.split(',').map(&:to_i)
 computer = Computer.new(PROGRAM)
-INSTRUCTIONS = open('program').read.each_char.map(&:ord)
+INSTRUCTIONS = open(ARGV[0]).read.each_char.map(&:ord)
 computer.add_inputs(INSTRUCTIONS)
 computer.run
 output = computer.read_and_clear_output
