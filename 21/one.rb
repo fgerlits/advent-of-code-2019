2f67edac6ea50dcbec9b0e0c253ea46a7817418f
Ferenc Gerlits <fgerlits@gmail.com>
2019-12-21 10:25:28 +0100

    Day 21 puzzle one

---
+++ 21/one.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require_relative '../common/intcode_computer'
+
+PROGRAM = open('input').read.split(',').map(&:to_i)
+computer = Computer.new(PROGRAM)
+INSTRUCTIONS = open('program').read.each_char.map(&:ord)
+computer.add_inputs(INSTRUCTIONS)
+computer.run
+output = computer.read_and_clear_output
+result = output.pop
+puts output.map(&:chr).join
+puts
+puts result
